61f202aca132716517072946b6d08014309628c7
EEDAT <EEDAT@TEB114105>
2023-03-03 14:31:41 +0100

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uppgift_61_
+{
+    public class Adderare
+    {
+        static void Main(string[] args)
+        {
+            int summa = Addera(1, 2, 3);
+            Console.WriteLine("Svaret är: " + summa);
+        }
+
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            int summa = tal1 + tal2 + tal3;
+            return summa;
+        }
+    }
+}
